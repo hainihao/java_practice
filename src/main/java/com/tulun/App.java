a86f2953bba5af3f065b543d70ff395ababc8537
haibihao <1452672867@qq.com>
2018-11-26 20:09:26 +0800

    //first

---
+++ src/main/java/com/tulun/App.java
@@ -0,0 +1,18 @@
+package com.tulun;
+
+import com.tulun.BookList.BookList;
+import com.tulun.BookSystem_JFrame.HomePage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+
+
+
+        HomePage a=new HomePage(new BookList());
+    }
+}
+
---
+++ src/main/java/com/tulun/test/test1.java
@@ -0,0 +1,17 @@
+package com.tulun.test;
+
+/**
+ * @author:liguozheng
+ * @Date:2018/10/19
+ * @time:21:45
+ * @description:
+ */
+
+public class test1 {
+    public static void main(String[] args) {
+        String a = "he ";
+        a= a+"  nihao";
+
+        System.out.println(a);
+    }
+}
