fcca0e0d5cbf339ee2ea9e69d22f48e6d3ff5fbc
haibihao <1452672867@qq.com>
2018-11-26 20:33:26 +0800

    //IODamo

--- src/main/java/com/tulun/src1/IODome.java
+++ src/main/java/com/tulun/src1/IODome.java
@@ -2,6 +2,7 @@ package com.tulun.src1;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Arrays;
 
 /**
@@ -16,14 +17,25 @@ public class IODome {
     public static void main(String[] args) {
 
         File file = new File("/Users/liguozheng/Documents/Linux/nibuhao.txt");
+        FileInputStream fileInputStream = null;
 
         try {
-            FileInputStream fileInputStream = new FileInputStream(file);
-            byte[] bytes = new byte[100];
-            fileInputStream.read(bytes);
-            System.out.println(Arrays.toString(bytes));
+            fileInputStream = new FileInputStream(file);
+//            byte[] bytes = new byte[100];
+//            fileInputStream.read(bytes);
+//            System.out.println(Arrays.toString(bytes));
+            int i ;
+            while ((i = fileInputStream.read())!=-1){
+                System.out.print((char)i);
+            }
         } catch (Exception e) {
             e.printStackTrace();
+        }finally{
+            try {
+                fileInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
