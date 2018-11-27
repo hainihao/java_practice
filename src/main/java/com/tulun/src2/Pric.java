318ac56c887ebc35f856772582c8c3b4939619ef
haibihao <1452672867@qq.com>
2018-11-27 21:55:30 +0800

    //IODamo

--- src/main/java/com/tulun/src2/Pric.java
+++ src/main/java/com/tulun/src2/Pric.java
@@ -1,9 +1,7 @@
 package com.tulun.src2;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
 
 /**
  * @author:liguozheng
@@ -14,12 +12,48 @@ import java.io.IOException;
 @SuppressWarnings("all")
 public class Pric {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         String reader = "/Users/liguozheng/Documents/Linux/nibuhao.txt";
         String wtiter = "/Users/liguozheng/Documents/Linux/ok.txt";
-        String s= reader(reader);
-        writer(wtiter,s);
+//        String s= reader(reader);
+//        writer(wtiter,s);
+
+        File file = new File("./src");
+
+        System.out.println(file.exists());  //判断该路径是否正确
+
+        System.out.println(file.isFile());  //判断该路径是否为文件
+
+        System.out.println(file.isDirectory());   //判断该路径是否为目录
+
+        System.out.println(file.isHidden());      //判断该路径是否为隐藏文件
+
+//        file.deleteOnExit();    //在程序退出时删除
+//
+     //   file.delete();             //删除文件
+//        file.mkdir();
+//
+//        file.getName();
+
+        File[] files = file.listFiles();
+        String[] list = file.list();
+
+        System.out.println(Arrays.toString(list));
+
+
+        System.out.println(file.length());
+        for (File f : files){
+            System.out.println(f.getName());
+        }
+
+//        System.out.println(file.getAbsolutePath());
+//        try {
+//            System.out.println(file.createNewFile());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        searchFile("/Users/liguozheng/Documents/Java","java");
     }
 
     public static void writer(String path,String s){
@@ -66,5 +100,36 @@ public class Pric {
             }
         }
         return s.toString();
+    }
+
+    public static void searchFile(String path,String filter) throws Exception{
+
+        File file = new File(path);
+
+        if (!file.exists()){
+            throw new Exception("该路径输入错误 ! ! !");
+        }
+
+        if (file.listFiles()==null){
+            return;
+        }
+        getFilesList(file,filter);
+    }
+
+    public static void getFilesList(File file, String fileName){
+        if(file.exists()){
+            if(file.isDirectory()){
+                File[] files = file.listFiles();
+                if(files!=null){
+                    for(File f:files){
+                        getFilesList(f,fileName);
+                    }
+                }
+            }else if(file.isFile()){
+                if(file.getName().contains(fileName)){
+                    System.out.println(file.getPath());
+                }
+            }
+        }
     }
 }
