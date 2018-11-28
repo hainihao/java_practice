04c2eaf430b05f99dbef5b646e26bfe694c42440
haibihao <1452672867@qq.com>
2018-11-28 19:03:43 +0800

    //IODamo

--- src/main/java/com/tulun/src2/Pric.java
+++ src/main/java/com/tulun/src2/Pric.java
@@ -180,7 +180,7 @@ public class Pric {
 
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            stringBuilder.append(' ');
+            stringBuilder.append('-');
         }
         return stringBuilder.toString();
     }
