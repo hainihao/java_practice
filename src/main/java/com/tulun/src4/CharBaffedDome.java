fcb9c84f89e714bff61cf571154f4c1043e84202
haibihao <1452672867@qq.com>
2018-12-02 19:26:33 +0800

    //ioDome3

--- src/main/java/com/tulun/src4/CharBaffedDome.java
+++ src/main/java/com/tulun/src4/CharBaffedDome.java
@@ -1,5 +1,16 @@
 package com.tulun.src4;
 
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.SortedMap;
+
 /**
  * @author:liguozheng
  * @Date:2018/12/1
@@ -9,7 +20,46 @@ package com.tulun.src4;
 
 public class CharBaffedDome {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CharacterCodingException {
+        String path = "/Users/liguozheng/Documents/Linux/test.txt";
+       // inPut(path);
+        CharsetDome();
+    }
 
+    public static void inPut(String path){
+
+        File file = new File(path);
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(path));
+           // InputStreamReader inputStreamReader1 = new InputStreamReader(inputStreamReader);
+            Charset charset = Charset.defaultCharset();
+            SortedMap<String, Charset> stringCharsetSortedMap = Charset.availableCharsets();
+            Iterator<Charset> iterator = stringCharsetSortedMap.values().iterator();
+            while (iterator.hasNext()){
+                Charset next = iterator.next();
+                System.out.println(next);
+            }
+            System.out.println();
+            System.out.println(stringCharsetSortedMap.size());
+            System.out.println();
+            System.out.println(charset);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void CharsetDome() throws CharacterCodingException {
+
+        String s = "utf-8";
+        Charset charset = Charset.forName(s);
+        CharsetEncoder charsetEncoder = charset.newEncoder();
+        CharsetDecoder charsetDecoder = charset.newDecoder();
+        CharBuffer nihaoa = CharBuffer.wrap("挖到一枚afsfafsa");
+        ByteBuffer encode = charsetEncoder.encode(nihaoa);
+        System.out.println(encode);
+
+        CharBuffer decode =  charsetDecoder.decode(encode);
+        String string = decode.toString();
+        System.out.println(string);
     }
 }
---
+++ src/main/java/com/tulun/src4/RamdonFileDome.java
@@ -0,0 +1,51 @@
+package com.tulun.src4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * @author:liguozheng
+ * @Date:2018/12/1
+ * @time:11:48
+ * @description:
+ */
+
+public class RamdonFileDome {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String path = "/Users/liguozheng/Documents/Linux/nibuhao.txt";
+        inPut(path);
+        //reader(path);
+    }
+
+    public static void reader(String path){
+
+        try {
+            RandomAccessFile randomAccessFile = new RandomAccessFile(path,"r");
+            long filePointer = randomAccessFile.getFilePointer();
+            System.out.println(filePointer);
+            long length = randomAccessFile.length();
+            System.out.println(length);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void inPut(String path) throws FileNotFoundException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile(path,"rw");
+        try {
+            randomAccessFile.write(97);
+            randomAccessFile.write(98);
+            randomAccessFile.write(99);
+            randomAccessFile.write(100);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
