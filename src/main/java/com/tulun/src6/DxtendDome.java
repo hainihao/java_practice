0618d6235500212611ce4b02519c313e1ef7739e
haibihao <1452672867@qq.com>
2018-12-07 21:32:33 +0800

    //threadDome1

---
+++ src/main/java/com/tulun/src6/DxtendDome.java
@@ -0,0 +1,19 @@
+package com.tulun.src6;
+
+/**
+ * @author:liguozheng
+ * @Date:2018/12/7
+ * @time:21:27
+ * @description:
+ */
+
+public class DxtendDome {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void dome1(){
+
+    }
+}
