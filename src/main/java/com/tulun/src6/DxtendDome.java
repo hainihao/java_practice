e4467c17e065b07640d5be206a5365268ae11388
haibihao <1452672867@qq.com>
2018-12-08 10:08:22 +0800

    //ThreadDome1

--- src/main/java/com/tulun/src6/DxtendDome.java
+++ src/main/java/com/tulun/src6/DxtendDome.java
@@ -10,10 +10,12 @@ package com.tulun.src6;
 public class DxtendDome {
 
     public static void main(String[] args) {
+        dome1();
 
     }
 
     public static void dome1(){
-
+        Runnable runnable = () -> System.out.println("nihap");
+        runnable.run();
     }
 }
