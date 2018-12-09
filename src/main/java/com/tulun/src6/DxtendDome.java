9164f45db23b9f52e11a80e4af7ccb2b796f1e39
haibihao <1452672867@qq.com>
2018-12-09 20:36:03 +0800

    //ThreadDOme2

--- src/main/java/com/tulun/src6/DxtendDome.java
+++ src/main/java/com/tulun/src6/DxtendDome.java
@@ -1,5 +1,9 @@
 package com.tulun.src6;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * @author:liguozheng
  * @Date:2018/12/7
@@ -15,7 +19,40 @@ public class DxtendDome {
     }
 
     public static void dome1(){
-        Runnable runnable = () -> System.out.println("nihap");
-        runnable.run();
+
     }
+}
+
+class thread1 implements Runnable {
+
+
+
+    @Override
+    public void run() {
+
+    }
+}
+
+
+@Getter
+@Setter
+@ToString
+class Student{
+
+    private int id;
+    private String name;
+    private int age;
+    private int[] score;
+
+    public Student() {
+        this(7);
+    }
+
+    private Student(int size) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.score = new int[size];
+    }
+
 }
